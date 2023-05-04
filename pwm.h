37b64b7340b3c3487c4ba0dd32548d9de00c9519
OAS1234 <108501061+OAS1234@users.noreply.github.com>
2023-05-04 14:21:58 -0700

    Update pwm.h

--- pwm.h
+++ pwm.h
@@ -1,20 +1,23 @@
-// Author:  Ritik Makhija
-// Net ID:   ritikmakhija
-// Date:04/27\2023
-// Assignment: Class Project
+// Author:  Osman Syed
+// Net ID:         osmansyed
+// Date:04/06\2003
+// Assignment: Lab 4
 //
-
 
 
 #ifndef PWM_H
 #define PWM_H
 
-#include <avr/io.h>
+void initPWMTimer1();//this is from example code. Don't need it
+void servoinit(); //timer
+void initPWMTimer3(); //needed as per lab requirements
+void init_L293D();
+void L293D_clockwise();
+void L293D_counter_clockwise();
+void STOP_motor ();
+void changeDutyCycle(unsigned int result);
 
-void initTemperatureHumidityPWM();
 
-void setTemperatureHumidityFan(float temperature, float humidity);
-
-void clearTemperatureHumidityFan();
+void setTemperatureHumidityFan(float temperature);
 
 #endif
